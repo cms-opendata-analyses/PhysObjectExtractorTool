eddf5e106438a4dae09cd59883a2b1ddf01ac55d
asdru30 <45265594+asdru30@users.noreply.github.com>
2019-01-22 17:03:46 -0500

    Update PhotonAnalyzer.cc

--- PhysicsObjects/photon/src/PhotonAnalyzer.cc
+++ PhysicsObjects/photon/src/PhotonAnalyzer.cc
@@ -111,7 +111,7 @@ PhotonAnalyzer::PhotonAnalyzer(const edm::ParameterSet& iConfig)
 	hist_ch =  fs->make <TH1D>("hist_ch", "Electron ch ", 100,0,5000 );
 	photonhisto = fs->make <TH1D>("photonhisto", "Photon histo", 100, 0, 5000);
 
-	InputphotonInput = iConfig.getParameter<edm::InputTag>("InputCollection");
+	photonInput = iConfig.getParameter<edm::InputTag>("InputCollection");
 
 }
 
@@ -160,29 +160,29 @@ PhotonAnalyzer::analyzePhotons(const edm::Event& iEvent, const edm::Handle<reco:
 	  _phi.clear();
 	  _ch.clear();
 
-  if(objeto.isValid()){
+  if(photons.isValid()){
      // get the number of photons in the event
      numphoton=(*photons).size();
-     objetohisto->Fill(objeto->size());
-        for (reco::PhotonCollection::const_iterator itobjeto=objeto->begin(); itobjeto!=objeto->end(); ++itobjeto){
+     photonhisto->Fill(photons->size());
+        for (reco::PhotonCollection::const_iterator itphoton=photons->begin(); itphoton!=photons->end(); ++itphoton){
 
-	    _e.push_back(itobjeto->energy());
-	    _pt.push_back(itobjeto->pt());
-	    _px.push_back(itobjeto->px());
-	    _py.push_back(itobjeto->py());
-	    _pz.push_back(itobjeto->pz());
-	    _eta.push_back(itobjeto->eta());
-	    _phi.push_back(itobjeto->phi());
-	    _ch.push_back(itobjeto->charge());
+	    _e.push_back(itphoton->energy());
+	    _pt.push_back(itphoton->pt());
+	    _px.push_back(itphoton->px());
+	    _py.push_back(itphoton->py());
+	    _pz.push_back(itphoton->pz());
+	    _eta.push_back(itphoton->eta());
+	    _phi.push_back(itphoton->phi());
+	    _ch.push_back(itphoton->charge());
 
-	    hist_e->Fill(itobjeto->energy());
-	    hist_pt->Fill(itobjeto->pt());
-	    hist_px->Fill(itobjeto->px());
-	    hist_py->Fill(itobjeto->py());
-	    hist_pz->Fill(itobjeto->pz());
-	    hist_eta->Fill(itobjeto->eta());
-	    hist_phi->Fill(itobjeto->phi());
-	    hist_ch->Fill(itobjeto->charge());
+	    hist_e->Fill(itphoton->energy());
+	    hist_pt->Fill(itphoton->pt());
+	    hist_px->Fill(itphoton->px());
+	    hist_py->Fill(itphoton->py());
+	    hist_pz->Fill(itphoton->pz());
+	    hist_eta->Fill(itphoton->eta());
+	    hist_phi->Fill(itphoton->phi());
+	    hist_ch->Fill(itphoton->charge());
 
         }
   }
@@ -196,17 +196,17 @@ void
 PhotonAnalyzer::beginJob()
 {
 
-mfile = new TFile("ObjetoInfo.root","RECREATE");
-mtree = new TTree("mtree","Objeto information");
+mfile = new TFile("PhotonInfo.root","RECREATE");
+mtree = new TTree("mtree","Photon information");
 
-  mytree->Branch("_e",&_e);
-  mytree->Branch("_pt",&_pt);
-  mytree->Branch("_px",&_px);
-  mytree->Branch("_py",&_py);
-  mytree->Branch("_pz",&_pz);
-  mytree->Branch("_eta",&_eta);
-  mytree->Branch("_phi",&_phi);
-  mytree->Branch("_ch",&_ch);
+  mytree->Branch("photon_e",&_e);
+  mytree->Branch("photon_pt",&_pt);
+  mytree->Branch("photon_px",&_px);
+  mytree->Branch("photon_py",&_py);
+  mytree->Branch("photon_pz",&_pz);
+  mytree->Branch("photon_eta",&_eta);
+  mytree->Branch("photon_phi",&_phi);
+  mytree->Branch("photon_ch",&_ch);
 
 }
 
