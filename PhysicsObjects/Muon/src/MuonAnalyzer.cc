7645675c2355446bae67c4445f8fa8e8b5b9cba3
asdru30 <45265594+asdru30@users.noreply.github.com>
2019-01-22 16:33:18 -0500

    Update MuonAnalyzer.cc

--- PhysicsObjects/Muon/src/MuonAnalyzer.cc
+++ PhysicsObjects/Muon/src/MuonAnalyzer.cc
@@ -109,9 +109,9 @@ MuonAnalyzer::MuonAnalyzer(const edm::ParameterSet& iConfig)
 	hist_eta = fs->make <TH1D>("hist_eta", "Electron eta ", 100, 0, 5000 );
 	hist_phi = fs->make <TH1D>("hist_phi", "Electron phi ", 100, 0, 5000 );
 	hist_ch =  fs->make <TH1D>("hist_ch", "Electron ch ", 100,0,5000 );
-	histo = fs->make <TH1D>("objetohisto", "objeto histo", 100, 0, 5000);
+	muonhisto = fs->make <TH1D>("muonhisto", "Muon histo", 100, 0, 5000);
 
-	Input = iConfig.getParameter<edm::InputTag>("InputCollection");
+	muonInput = iConfig.getParameter<edm::InputTag>("InputCollection");
 
 }
 
