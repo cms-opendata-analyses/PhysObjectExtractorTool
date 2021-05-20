24f66cec02da7cfcb7e7b135f79d2608e18f13e7
anniinakinnunen <anniina.he.kinnunen@gmail.com>
2021-05-20 12:19:19 +0300

    add workflow

---
+++ commands.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+sudo chown $USER /mnt/vol
+
+mkdir workspace
+cd workspace
+# For the plain github action with docker, the area would be available in /mnt/vol
+cp -r /mnt/vol PhysObjectExtractorTool
+cd PhysObjectExtractorTool
+cd PhysObjectExtractor
+scram b
+
+ln -s python/physicsobjectsinfo_cfg.py .
+
+sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" physicsobjectsinfo_cfg.py
+cmsRun physicsobjectsinfo_cfg.py
+
+cp output.root /mnt/vol/
+echo ls -l /mnt/vol
+ls -l /mnt/vol
