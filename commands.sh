0bc5712fe73dc709a2bb6dd259545b4b6dfa0ac2
anniinakinnunen <anniina.he.kinnunen@gmail.com>
2021-05-20 15:11:31 +0300

    test workflow output

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
+# Parameters: $1 number of events (default: x), $2 configuration file (default: physicsobjectsinfo_cfg.py)
 sudo chown $USER /mnt/vol
 
 mkdir workspace
@@ -14,6 +15,6 @@ ln -s python/physicsobjectsinfo_cfg.py .
 sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" physicsobjectsinfo_cfg.py
 cmsRun physicsobjectsinfo_cfg.py
 
-cp *.root /mnt/vol/
+cp */*.root /mnt/vol/
 echo ls -l /mnt/vol
 ls -l /mnt/vol
