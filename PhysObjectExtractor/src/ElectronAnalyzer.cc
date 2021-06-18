93961f6e9f99129a44ec64c2c76a2f040443813b
Nikolas Pervan <nikolas_pervan@brown.edu>
2021-06-18 04:18:53 +0200

    Add JEC, electron, photon updates to modular branch

--- PhysObjectExtractor/src/ElectronAnalyzer.cc
+++ PhysObjectExtractor/src/ElectronAnalyzer.cc
@@ -19,6 +19,10 @@
 //classes to extract electron information
 #include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "RecoEgamma/EgammaTools/interface/ConversionTools.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
 
 //classes to save data
 #include "TTree.h"
@@ -30,37 +34,41 @@
 //
 
 class ElectronAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit ElectronAnalyzer(const edm::ParameterSet&);
-      ~ElectronAnalyzer();
+public:
+  explicit ElectronAnalyzer(const edm::ParameterSet&);
+  ~ElectronAnalyzer();
+  
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  
+private:
+  virtual void beginJob() ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+  virtual void endRun(edm::Run const&, edm::EventSetup const&);
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-      //declare the input tag for GsfElectronCollection
-      edm::InputTag electronInput;
-	
-      // ----------member data ---------------------------
-
-
-      TTree *mtree;
-      int numelectron; //number of electrons in the event
-      std::vector<float> electron_e;
-      std::vector<float> electron_pt;
-      std::vector<float> electron_px;
-      std::vector<float> electron_py;
-      std::vector<float> electron_pz;
-      std::vector<float> electron_eta;
-      std::vector<float> electron_phi;
-      std::vector<float> electron_ch;
+  //declare the input tag for GsfElectronCollection
+  edm::InputTag electronInput;
+  
+  // ----------member data ---------------------------
+  
+  
+  TTree *mtree;
+  int numelectron; //number of electrons in the event
+  std::vector<float> electron_e;
+  std::vector<float> electron_pt;
+  std::vector<float> electron_px;
+  std::vector<float> electron_py;
+  std::vector<float> electron_pz;
+  std::vector<float> electron_eta;
+  std::vector<float> electron_phi;
+  std::vector<float> electron_ch;
+  std::vector<float> electron_iso;
+  std::vector<bool> electron_isLoose;
+  std::vector<bool> electron_isMedium;
+  std::vector<bool> electron_isTight;
 };
 
 //
@@ -78,30 +86,37 @@ class ElectronAnalyzer : public edm::EDAnalyzer {
 ElectronAnalyzer::ElectronAnalyzer(const edm::ParameterSet& iConfig)
 {
 //now do what ever initialization is needed
-	electronInput = iConfig.getParameter<edm::InputTag>("InputCollection");
-	edm::Service<TFileService> fs;
-	mtree = fs->make<TTree>("Events", "Events");
-
-
-	mtree->Branch("numberelectron",&numelectron);
-	mtree->GetBranch("numberelectron")->SetTitle("number of electrons");
-	mtree->Branch("electron_e",&electron_e);
-	mtree->GetBranch("electron_e")->SetTitle("electron energy");
-	mtree->Branch("electron_pt",&electron_pt);
-	mtree->GetBranch("electron_pt")->SetTitle("electron transverse momentum");
- 	mtree->Branch("electron_px",&electron_px);
-	mtree->GetBranch("electron_px")->SetTitle("electron momentum x-component");
- 	mtree->Branch("electron_py",&electron_py);
-	mtree->GetBranch("electron_py")->SetTitle("electron momentum y-component");
-	mtree->Branch("electron_pz",&electron_pz);
-	mtree->GetBranch("electron_pz")->SetTitle("electron momentum z-component");
- 	mtree->Branch("electron_eta",&electron_eta);
-	mtree->GetBranch("electron_eta")->SetTitle("electron pseudorapidity");
-	mtree->Branch("electron_phi",&electron_phi);
-	mtree->GetBranch("electron_phi")->SetTitle("electron polar angle");
-	mtree->Branch("electron_ch",&electron_ch);
-	mtree->GetBranch("electron_ch")->SetTitle("electron charge");
-	
+  electronInput = iConfig.getParameter<edm::InputTag>("InputCollection");
+  edm::Service<TFileService> fs;
+  mtree = fs->make<TTree>("Events", "Events");
+  
+  
+  mtree->Branch("numberelectron",&numelectron);
+  mtree->GetBranch("numberelectron")->SetTitle("number of electrons");
+  mtree->Branch("electron_e",&electron_e);
+  mtree->GetBranch("electron_e")->SetTitle("electron energy");
+  mtree->Branch("electron_pt",&electron_pt);
+  mtree->GetBranch("electron_pt")->SetTitle("electron transverse momentum");
+  mtree->Branch("electron_px",&electron_px);
+  mtree->GetBranch("electron_px")->SetTitle("electron momentum x-component");
+  mtree->Branch("electron_py",&electron_py);
+  mtree->GetBranch("electron_py")->SetTitle("electron momentum y-component");
+  mtree->Branch("electron_pz",&electron_pz);
+  mtree->GetBranch("electron_pz")->SetTitle("electron momentum z-component");
+  mtree->Branch("electron_eta",&electron_eta);
+  mtree->GetBranch("electron_eta")->SetTitle("electron pseudorapidity");
+  mtree->Branch("electron_phi",&electron_phi);
+  mtree->GetBranch("electron_phi")->SetTitle("electron polar angle");
+  mtree->Branch("electron_ch",&electron_ch);
+  mtree->GetBranch("electron_ch")->SetTitle("electron charge");
+  mtree->Branch("electron_iso",&electron_iso);
+  mtree->GetBranch("electron_iso")->SetTitle("electron isolation");
+  mtree->Branch("electron_isLoose",&electron_isLoose);
+  mtree->GetBranch("electron_isLoose")->SetTitle("electron tagged loose");
+  mtree->Branch("electron_isMedium",&electron_isMedium);
+  mtree->GetBranch("electron_isMedium")->SetTitle("electron tagged medium");
+  mtree->Branch("electron_isTight",&electron_isTight);
+  mtree->GetBranch("electron_isTight")->SetTitle("electron tagged tight");
 }
 
 ElectronAnalyzer::~ElectronAnalyzer()
@@ -123,6 +138,14 @@ ElectronAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
    Handle<reco::GsfElectronCollection> myelectrons;
    iEvent.getByLabel(electronInput, myelectrons);
+   Handle<reco::ConversionCollection> hConversions;
+   iEvent.getByLabel("allConversions", hConversions);
+   Handle<reco::BeamSpot> bsHandle;
+   iEvent.getByLabel("offlineBeamSpot", bsHandle);
+   const reco::BeamSpot &beamspot = *bsHandle.product();
+   Handle<reco::VertexCollection> vertices;
+   iEvent.getByLabel(InputTag("offlinePrimaryVertices"), vertices);
+   math::XYZPoint pv(vertices->begin()->position());
 
    numelectron = 0;
    electron_e.clear();
@@ -133,21 +156,74 @@ ElectronAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
    electron_eta.clear();
    electron_phi.clear();
    electron_ch.clear();
+   electron_iso.clear();
+   electron_isLoose.clear();
+   electron_isMedium.clear();
+   electron_isTight.clear();
 
    if(myelectrons.isValid()){
      // get the number of electrons in the event
      numelectron=myelectrons->size();
-        for (reco::GsfElectronCollection::const_iterator itElec=myelectrons->begin(); itElec!=myelectrons->end(); ++itElec){
+     float el_pfIso = -999;
+     for (reco::GsfElectronCollection::const_iterator itElec=myelectrons->begin(); itElec!=myelectrons->end(); ++itElec){
+       int missing_hits = itElec->gsfTrack()->trackerExpectedHitsInner().numberOfHits()-itElec->gsfTrack()->hitPattern().numberOfHits();
+       bool passelectronveto = !ConversionTools::hasMatchedConversion(*itElec, hConversions, beamspot.position());
+       if (itElec->passingPflowPreselection()) {
+	 auto iso03 = itElec->pfIsolationVariables();
+	 el_pfIso = (iso03.chargedHadronIso + iso03.neutralHadronIso + iso03.photonIso)/itElec->pt();
+       } 
+       auto trk = itElec->gsfTrack();
+       bool isLoose = false, isMedium = false, isTight = false;
+       if ( abs(itElec->eta()) <= 1.479 ) {   
+	 if ( abs(itElec->deltaEtaSuperClusterTrackAtVtx())<.007 && abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.15 && 
+	      itElec->sigmaIetaIeta()<.01 && itElec->hadronicOverEm()<.12 && 
+	      abs(trk->dxy(pv))<.02 && abs(trk->dz(pv))<.2 && 
+	                missing_hits<=1 && el_pfIso<.15 && passelectronveto==true &&
+	      abs(1/itElec->ecalEnergy()-1/(itElec->ecalEnergy()/itElec->eSuperClusterOverP()))<.05 ){
+	       
+	   isLoose = true;
+	       
+	   if ( abs(itElec->deltaEtaSuperClusterTrackAtVtx())<.004 && abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.06 && abs(trk->dz(pv))<.1 ){
+	     isMedium = true;
+	             
+	     if (abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.03 && missing_hits<=0 && el_pfIso<.10 ){
+	       isTight = true;
+	     }
+	   }
+	 }
+       }
+       else if ( abs(itElec->eta()) > 1.479 && abs(itElec->eta()) < 2.5 ) {
+	 if ( abs(itElec->deltaEtaSuperClusterTrackAtVtx())<.009 && abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.1 && 
+	      itElec->sigmaIetaIeta()<.03 && itElec->hadronicOverEm()<.1 && 
+	      abs(trk->dxy(pv))<.02 && abs(trk->dz(pv))<.2 && 
+	                missing_hits<=1 && el_pfIso<.15 && passelectronveto==true &&
+	      abs(1/itElec->ecalEnergy()-1/(itElec->ecalEnergy()/itElec->eSuperClusterOverP()))<.05) {
+	       
+	   isLoose = true;
+	       
+	   if ( abs(itElec->deltaEtaSuperClusterTrackAtVtx())<.007 && abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.03 && abs(trk->dz(pv))<.1 ){
+	     isMedium = true;
+	             
+	     if ( abs(itElec->deltaEtaSuperClusterTrackAtVtx())<.005 && abs(itElec->deltaPhiSuperClusterTrackAtVtx())<.02 && missing_hits<=0 && el_pfIso<.10 ){
+	       isTight = true;
+	     }
+	   }
+	 }
+       }
 
-    	        electron_e.push_back(itElec->energy());
-    	        electron_pt.push_back(itElec->pt());
-    	        electron_px.push_back(itElec->px());
-    	        electron_py.push_back(itElec->py());
-    	        electron_pz.push_back(itElec->pz());
-    	        electron_eta.push_back(itElec->eta());
-    	        electron_phi.push_back(itElec->phi());
-    	        electron_ch.push_back(itElec->charge());
-        }
+       electron_e.push_back(itElec->energy());
+       electron_pt.push_back(itElec->pt());
+       electron_px.push_back(itElec->px());
+       electron_py.push_back(itElec->py());
+       electron_pz.push_back(itElec->pz());
+       electron_eta.push_back(itElec->eta());
+       electron_phi.push_back(itElec->phi());
+       electron_ch.push_back(itElec->charge());
+       electron_iso.push_back(el_pfIso);
+       electron_isLoose.push_back(isLoose);
+       electron_isMedium.push_back(isMedium);
+       electron_isTight.push_back(isTight);
+     }
   }
 
   mtree->Fill();
--- PhysObjectExtractor/src/PhotonAnalyzer.cc
+++ PhysObjectExtractor/src/PhotonAnalyzer.cc
@@ -19,6 +19,9 @@
 //classes to extract Photon information
 #include "DataFormats/EgammaCandidates/interface/Photon.h"
 #include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "RecoEgamma/EgammaTools/interface/ConversionTools.h"
 
 //classes to save data
 #include "TTree.h"
@@ -30,36 +33,40 @@
 //
 
 class PhotonAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit PhotonAnalyzer(const edm::ParameterSet&);
-      ~PhotonAnalyzer();
+public:
+  explicit PhotonAnalyzer(const edm::ParameterSet&);
+  ~PhotonAnalyzer();
+  
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+private:
+  virtual void beginJob() ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+  virtual void endRun(edm::Run const&, edm::EventSetup const&);
+  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+  
+  //declare the input tag for PhotonCollection
+  edm::InputTag photonInput;
 
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-
-      //declare the input tag for PhotonCollection
-      edm::InputTag photonInput;
-
-      // ----------member data ---------------------------
-
-      TTree *mtree;
-      int numphoton; //number of photons in the event
-      std::vector<float> photon_e;
-      std::vector<float> photon_pt;
-      std::vector<float> photon_px;
-      std::vector<float> photon_py;
-      std::vector<float> photon_pz;
-      std::vector<float> photon_eta;
-      std::vector<float> photon_phi;
-      std::vector<float> photon_ch;
+  // ----------member data ---------------------------
+  
+  TTree *mtree;
+  int numphoton; //number of photons in the event
+  std::vector<float> photon_e;
+  std::vector<float> photon_pt;
+  std::vector<float> photon_px;
+  std::vector<float> photon_py;
+  std::vector<float> photon_pz;
+  std::vector<float> photon_eta;
+  std::vector<float> photon_phi;
+  std::vector<float> photon_ch;
+  std::vector<float> photon_iso;
+  std::vector<bool>  photon_isLoose;
+  std::vector<bool>  photon_isMedium;
+  std::vector<bool>  photon_isTight;
 };
 
 //
@@ -78,20 +85,24 @@ PhotonAnalyzer::PhotonAnalyzer(const edm::ParameterSet& iConfig)
 
 {
 //now do what ever initialization is needed
-	photonInput = iConfig.getParameter<edm::InputTag>("InputCollection");
-	edm::Service<TFileService> fs;
-	mtree = fs->make<TTree>("Events", "Events");
-
-
-       mtree->Branch("numberphoton",&numphoton);
-       mtree->Branch("photon_e",&photon_e);
-       mtree->Branch("photon_pt",&photon_pt);
-       mtree->Branch("photon_px",&photon_px);
-       mtree->Branch("photon_py",&photon_py);
-       mtree->Branch("photon_pz",&photon_pz);
-       mtree->Branch("photon_eta",&photon_eta);
-       mtree->Branch("photon_phi",&photon_phi);
-       mtree->Branch("photon_ch",&photon_ch);
+  photonInput = iConfig.getParameter<edm::InputTag>("InputCollection");
+  edm::Service<TFileService> fs;
+  mtree = fs->make<TTree>("Events", "Events");
+  
+  
+  mtree->Branch("numberphoton",&numphoton);
+  mtree->Branch("photon_e",&photon_e);
+  mtree->Branch("photon_pt",&photon_pt);
+  mtree->Branch("photon_px",&photon_px);
+  mtree->Branch("photon_py",&photon_py);
+  mtree->Branch("photon_pz",&photon_pz);
+  mtree->Branch("photon_eta",&photon_eta);
+  mtree->Branch("photon_phi",&photon_phi);
+  mtree->Branch("photon_ch",&photon_ch);
+  mtree->Branch("photon_iso",&photon_iso);
+  mtree->Branch("photon_isLoose",&photon_isLoose);
+  mtree->Branch("photon_isMedium",&photon_isMedium);
+  mtree->Branch("photon_isTight",&photon_isTight);
 }
 
 
@@ -114,7 +125,17 @@ PhotonAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
    Handle<reco::PhotonCollection> myphotons;
    iEvent.getByLabel(photonInput, myphotons);
-   
+   Handle<reco::GsfElectronCollection> electrons;
+   iEvent.getByLabel(InputTag("gsfElectrons"), electrons);
+   Handle<reco::ConversionCollection> hConversions;
+   iEvent.getByLabel("allConversions", hConversions);
+   Handle<reco::BeamSpot> bsHandle;
+   iEvent.getByLabel("offlineBeamSpot", bsHandle);
+   const reco::BeamSpot &beamspot = *bsHandle.product();
+   Handle<double> rhoHandle;
+   iEvent.getByLabel(InputTag("fixedGridRhoAll"), rhoHandle);
+   double rhoIso = std::max(*(rhoHandle.product()), 0.0);   
+
    numphoton = 0;
    photon_e.clear();
    photon_pt.clear();
@@ -124,21 +145,102 @@ PhotonAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    photon_eta.clear();
    photon_phi.clear();
    photon_ch.clear();
+   photon_iso.clear();
+   photon_isLoose.clear();
+   photon_isMedium.clear();
+   photon_isTight.clear();
 
    if(myphotons.isValid()){
      // get the number of photons in the event
      numphoton=myphotons->size();
-        for (reco::PhotonCollection::const_iterator itphoton=myphotons->begin(); itphoton!=myphotons->end(); ++itphoton){
+     for (reco::PhotonCollection::const_iterator itphoton=myphotons->begin(); itphoton!=myphotons->end(); ++itphoton){
+       bool passelectronveto = !ConversionTools::hasMatchedPromptElectron(itphoton->superCluster(), electrons, hConversions, beamspot.position());
+       double scEta = (itphoton)->superCluster()->eta();
+       double CH_AEff, NH_AEff, Ph_AEff;
+       if(fabs(scEta) >2.4) {
+	 CH_AEff = 0.012;
+	 NH_AEff = 0.072;
+	 Ph_AEff = 0.266;
+       }
+       else if(fabs(scEta) >2.3) {
+	 CH_AEff = 0.020;
+	 NH_AEff = 0.039;
+	 Ph_AEff = 0.260;
+       } 
+       else if(fabs(scEta) >2.2) {
+	 CH_AEff = 0.016;
+	 NH_AEff = 0.024;
+	 Ph_AEff = 0.262;
+       } 
+       else if(fabs(scEta) >2.0) {
+	 CH_AEff = 0.012;
+	 NH_AEff = 0.015;
+	 Ph_AEff = 0.216;
+       } 
+       else if(fabs(scEta) >1.479) {
+	 CH_AEff = 0.014;
+	 NH_AEff = 0.039;
+	 Ph_AEff = 0.112;
+       } 
+       else if(fabs(scEta) >0.1) {
+	 CH_AEff = 0.010;
+	 NH_AEff = 0.057;
+	 Ph_AEff = 0.130;
+       } 
+       else {
+	 CH_AEff = 0.012;
+	 NH_AEff = 0.030;
+	 Ph_AEff = 0.148;
+       } 
+       double ph_hOverEm = itphoton->hadTowOverEm();
+       double ph_sigIetaIeta = itphoton->sigmaIetaIeta();
+       double ph_photonIso = itphoton->photonIso();
+       double corrPFCHIso = std::max(itphoton->chargedHadronIso() - rhoIso * CH_AEff, 0.);
+       double corrPFNHIso = std::max(itphoton->neutralHadronIso() - rhoIso * NH_AEff, 0.);
+       double corrPFPhIso = std::max(itphoton->photonIso() - rhoIso * Ph_AEff, 0.);
+       bool isLoose = false, isMedium = false, isTight = false;
+       if ( itphoton->eta() <= 1.479 ){
+	 if ( ph_hOverEm<.05 && ph_sigIetaIeta<.012 && 
+	      corrPFCHIso<2.6 && corrPFNHIso<(3.5+.04*itphoton->pt()) && 
+	      corrPFPhIso<(1.3+.005*itphoton->pt()) && passelectronveto==true) {
+	   isLoose = true;
 
-        	    photon_e.push_back(itphoton->energy());
-        	    photon_pt.push_back(itphoton->pt());
-        	    photon_px.push_back(itphoton->px());
-        	    photon_py.push_back(itphoton->py());
-        	    photon_pz.push_back(itphoton->pz());
-        	    photon_eta.push_back(itphoton->eta());
-        	    photon_phi.push_back(itphoton->phi());
-        	    photon_ch.push_back(itphoton->charge());
-        }
+	   if ( ph_sigIetaIeta<.011 && corrPFCHIso<1.5 && corrPFNHIso<(1.0+.04*itphoton->pt()) && corrPFPhIso<(.7+.005*itphoton->pt())){
+	     isMedium = true;
+
+	     if ( corrPFCHIso<.7 && corrPFNHIso<(.4+.04*itphoton->pt()) && corrPFPhIso<(.5+0.005*itphoton->pt()) ){
+	       isTight = true;
+	     }
+	   }
+	 }
+       }
+       else if ( itphoton->eta() > 1.479 && itphoton->eta() < 2.5 ) {
+	 if ( ph_hOverEm<.05 && ph_sigIetaIeta<.034 && corrPFCHIso<2.3 && corrPFNHIso<(2.9+.04*itphoton->pt()) && passelectronveto==true ){
+	   isLoose = true;
+	           
+	   if ( ph_sigIetaIeta<.033 && corrPFCHIso<1.2 && corrPFNHIso<(1.5+.04*itphoton->pt()) && corrPFPhIso<(1.0+.005*itphoton->pt())) {
+	     isMedium = true;
+
+	     if ( ph_sigIetaIeta<0.031 && corrPFCHIso<0.5){
+	       isTight = true;
+	     }
+	   }
+	 }
+       }
+       
+       photon_e.push_back(itphoton->energy());
+       photon_pt.push_back(itphoton->pt());
+       photon_px.push_back(itphoton->px());
+       photon_py.push_back(itphoton->py());
+       photon_pz.push_back(itphoton->pz());
+       photon_eta.push_back(itphoton->eta());
+       photon_phi.push_back(itphoton->phi());
+       photon_ch.push_back(itphoton->charge());
+       photon_iso.push_back(ph_photonIso);
+       photon_isLoose.push_back(isLoose);
+       photon_isMedium.push_back(isMedium);
+       photon_isTight.push_back(isTight);
+     }
   }
   
   mtree->Fill();
