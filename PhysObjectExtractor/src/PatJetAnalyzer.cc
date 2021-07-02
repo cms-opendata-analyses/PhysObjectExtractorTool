499f62a3eda31cc40b65bded100358c6e29240ea
sib37385 <sib37385@bethel.edu>
2021-07-02 16:40:16 +0200

    fixed PatJet

--- PhysObjectExtractor/src/PatJetAnalyzer.cc
+++ PhysObjectExtractor/src/PatJetAnalyzer.cc
@@ -138,10 +138,6 @@ PatJetAnalyzer::PatJetAnalyzer(const edm::ParameterSet& iConfig)
 
   // Make the FactorizedJetCorrector and Uncertainty
   jecUnc_ = boost::shared_ptr<JetCorrectionUncertainty>( new JetCorrectionUncertainty(jecUncName_) );
-  JetCorrectorParameters *ak5PFPar = new JetCorrectorParameters(jerResName_);
-  ak5PFCorrector = boost::shared_ptr<SimpleJetCorrector>( new SimpleJetCorrector(*ak5PFPar) );  
-  jer_ = boost::shared_ptr<SimpleJetCorrector>( new SimpleJetCorrector(*jerPar) );  
-
   mtree->Branch("numberjet",&numjet);
   mtree->GetBranch("numberjet")->SetTitle("Number of Jets");
   mtree->Branch("jet_pt",&jet_pt);
