26c43f37af1bc9db975532ef40238ca6d8d23472
Edgar <caredg@gmail.com>
2021-07-10 17:16:34 -0500

    remove comments in Loop function as it could be confusing

--- PhysObjectExtractor/test/EventLoopAnalysisTemplate.cxx
+++ PhysObjectExtractor/test/EventLoopAnalysisTemplate.cxx
@@ -183,29 +183,6 @@ Int_t EventLoopAnalysisTemplate::Cut(Long64_t entry)
 
 void EventLoopAnalysisTemplate::Loop()
 {
-//   In a ROOT session, you can do:
-//      Root > .L EventLoopAnalysisTemplate.C
-//      Root > EventLoopAnalysisTemplate t
-//      Root > t.GetEntry(12); // Fill t data members with entry number 12
-//      Root > t.Show();       // Show values of entry 12
-//      Root > t.Show(16);     // Read and show values of entry 16
-//      Root > t.Loop();       // Loop on all entries
-//
-
-//     This is the loop skeleton where:
-//    jentry is the global entry number in the chain
-//    ientry is the entry number in the current Tree
-//  Note that the argument to GetEntry must be:
-//    jentry for TChain::GetEntry
-//    ientry for TTree::GetEntry and TBranch::GetEntry
-//
-//       To read only selected branches, Insert statements like:
-// METHOD1:
-//    fChain->SetBranchStatus("*",0);  // disable all branches
-//    fChain->SetBranchStatus("branchname",1);  // activate branchname
-// METHOD2: replace line
-//    fChain->GetEntry(jentry);       //read all branches
-//by  b_branchname->GetEntry(ientry); //read only this branch
   if (fChain == 0) return;
 
     Long64_t nentries = fChain->GetEntriesFast();
