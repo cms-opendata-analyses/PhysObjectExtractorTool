b365235c9c87b9cf300eac23ac82de42e5698a6c
jrw46742 <79337499+jrw46742@users.noreply.github.com>
2021-07-17 12:54:33 -0500

    Fix Eta Protection (#54)
    * Fixed PAT on data, updated README
    
    * Fix README
    
    * Small cfg fix
    
    * Add JEC/JER examples to README, fix Jet Analyzers
    
    * Add JEC/JER examples for README, fix Jet Analyzers
    
    * Fixing for pull request
    
    * Fixing for pull request
    
    * Fixing merge conflicts...
    
    * Fixing merge conflicts...
    
    * Add jet eta protection
    
    * Fix Merge Conflicts
    
    * Fix Compiling Errors
    
    * Fix Eta Protection
    
    * Fix Merge Conflicts

--- PhysObjectExtractor/src/JetAnalyzer.cc
+++ PhysObjectExtractor/src/JetAnalyzer.cc
@@ -392,12 +392,12 @@ JetAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       corrUp = 1.0;
       corrDown = 1.0;
 
-      if( itjet->eta() < 5) jecUnc_->setJetEta( itjet->eta() );
+      if( fabs(itjet->eta()) < 5) jecUnc_->setJetEta( itjet->eta() );
       else jecUnc_->setJetEta( 4.99 );
       jecUnc_->setJetPt( itjet->pt() );
       corrUp = (1 + fabs(jecUnc_->getUncertainty(1)));
 
-      if( itjet->eta() < 5) jecUnc_->setJetEta( itjet->eta() );
+      if( fabs(itjet->eta()) < 5) jecUnc_->setJetEta( itjet->eta() );
       else jecUnc_->setJetEta( 4.99 );
       jecUnc_->setJetPt( itjet->pt() );
       corrDown = (1 - fabs(jecUnc_->getUncertainty(-1)));
