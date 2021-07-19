8fa6052d452af88511624d7c92a9c101d5a039ef
Edgar <caredg@gmail.com>
2021-07-19 18:24:29 -0500

    update to a more general and robust ELAT example

--- PhysObjectExtractor/test/EventLoopAnalysisTemplate.cxx
+++ PhysObjectExtractor/test/EventLoopAnalysisTemplate.cxx
@@ -5,13 +5,16 @@
 // The template shows the structre of a potential analysis code
 // where more TTree friends can be added with more physics objects.
 //
+// The analysis part is based on the RDataFrame analysis example by Stefan Wunch:
+// https://github.com/cms-opendata-analyses/HiggsTauTauNanoAODOutreachAnalysis
+//
 // Done with ROOT version 5.32/00
 // from TTree Events/Events
 // found on file: myoutput.root
 //
 //
 // Compile me with:
-// g++ -g -O3 -Wall -Wextra -o EventLoopAnalysis EventLoopAnaalysisTemplate.cxx $(root-config --cflags --libs)
+// g++ -std=c++11 -g -O3 -Wall -Wextra -o EventLoopAnalysis EventLoopAnalysisTemplate.cxx $(root-config --cflags --libs)
 /////////////////////////////////////////////////////////////////////
 
 //Include ROOT classes
@@ -23,75 +26,116 @@
 #include <TCanvas.h>
 #include <TH1F.h>
 #include "TLatex.h"
+#include "TStopwatch.h"
+
 //Include C++ classes
 #include <iostream>
 #include <vector>
-	
+#include <map>
+#include <string>
+#include <cmath>
+
 using namespace std;
 
-//book example histograms
-TH1F* lumib = new TH1F("lumib","Luminosity block",10000,0,10000);
-TH1F* h_nmu = new TH1F("h_nmu","Number of muons",200,0,200);
-TH1F* h_mu_e = new TH1F("h_mu_e","Muon Energy",2000,0,2000);
-TH1F* h_mu_pt = new TH1F("h_mu_pt","Muon p_{T}",2000,0,2000);
 
+/*
+ * Base path to local filesystem or to EOS containing the datasets
+ */
+//const std::string samplesBasePath = "root://eospublic.cern.ch//eos/opendata/cms/upload/od-workshop/ws2021/";
+const string samplesBasePath = "";
+
+//book example histograms for specific variables
+//copy them in the constructor if you add more
+const int nhists = 1;
+TH1F* data_npv = new TH1F("data_npv","Number of primary vertices",30,0,30);
+
+//Requiered trigger
+const string triggerRequest = "HLT_L2DoubleMu23_NoVertex_v*";
 
 // Fixed size dimensions of array or collections stored in the TTree if any.
 
 class EventLoopAnalysisTemplate {
 public :
 	
-   TTree          *fChain;   //!pointer to the analyzed TTree or TChain	
-   TTree	  *tmuons;  
-   //Add more trees for friendship
+  TTree          *fChain;   //!pointer to the analyzed TTree or TChain	
+  TTree          *ttrigger;
+  TTree          *tevents;
+  TTree          *tvertex;
+  TTree          *tmuons;
+  TTree          *ttaus;
+  TTree          *tmets;
+  Int_t          fCurrent; //!current Tree number in a TChain
 
-   Int_t           fCurrent; //!current Tree number in a TChain
+  //for managing files and weights
+  TString          labeltag;
+  TString          filename;
+  Float_t          theweight;
 
-   // Declaration of example leaf types
-   Int_t           run;
-   UInt_t          luminosityBlock;
-   ULong64_t	   event;
-   vector<float>   *muon_e;
-   vector<float>   *muon_pt;
-
-   // List of example branches
-   TBranch        *b_run;   //!
-   TBranch        *b_luminosityBlock;   //!
-   TBranch        *b_event;   //!
-   TBranch        *b_muon_e;   //!
-   TBranch        *b_muon_pt;   //!
-
-   EventLoopAnalysisTemplate(TTree *tree=0);
-   virtual ~EventLoopAnalysisTemplate();
-   virtual Int_t    Cut(Long64_t entry);
-   virtual Int_t    GetEntry(Long64_t entry);
-   virtual Long64_t LoadTree(Long64_t entry);
-   virtual void     Init(TTree *tree);
-   virtual void     Loop();
-   virtual Bool_t   Notify();
-   virtual void     Show(Long64_t entry = -1);
-   void analysis();
+  //array to keep histograms to be written and easily loop over them
+  TH1F            *hists[nhists];
+  
+  // Declaration of example leaf types
+  Int_t           run;
+  UInt_t          luminosityBlock;
+  ULong64_t	   event;
+  Int_t           PV_npvs;
+  std::map<std::string, int> *triggermap;
+  vector<float>   *muon_pt;
+  
+  // List of example branches
+  TBranch        *b_run;   //!
+  TBranch        *b_luminosityBlock;   //!
+  TBranch        *b_event;   //!
+  TBranch        *b_PV_npvs;   //!
+  TBranch        *b_triggermap;   //!
+  TBranch        *b_muon_pt;   //!
+  
+  EventLoopAnalysisTemplate(TString filename, TString labeltag, Float_t theweight);
+  virtual ~EventLoopAnalysisTemplate();
+  virtual Int_t    GetEntry(Long64_t entry);
+  virtual Long64_t LoadTree(Long64_t entry);
+  virtual void     Init(TTree *tree);
+  virtual void     Loop();
+  virtual Bool_t   Notify();
+  virtual void     Show(Long64_t entry = -1);
+  void analysis();
+  bool MinimalSelection();
+  
 };
 
-EventLoopAnalysisTemplate::EventLoopAnalysisTemplate(TTree *tree) : fChain(0)
+//Constructor of the analysis class
+EventLoopAnalysisTemplate::EventLoopAnalysisTemplate(TString thefile, TString thelabel, Float_t sampleweight) : fChain(0)
 {
+  //Prepare some info for the analysis object:
+  filename = thefile;
+  labeltag = thelabel;
+  theweight = sampleweight;
+  
+
+  //Load histograms easy access
+  //hast to be in agreement with above definitions.
+  hists[0] = data_npv;
+
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
-   if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("myoutput.root");
-      if (!f || !f->IsOpen()) {
-         f = new TFile("myoutput.root");
-      }
-      TDirectory * dir = (TDirectory*)f->Get("myoutput.root:/myevents");
-      dir->GetObject("Events",tree);
-
-      //Get trees for friendship
-      tmuons = (TTree*)f->Get("mymuons/Events");
-      
-      //Make friendship	
-      tree->AddFriend(tmuons);
-	
-   }
+   TTree* tree = 0;
+   TFile *f = TFile::Open(filename);
+   //trigger should go first as it is the more complicated one
+   tree = (TTree*)f->Get("mytriggers/Events");
+   //Get trees for friendship
+   tevents = (TTree*)f->Get("myevents/Events");
+   tvertex = (TTree*)f->Get("mypvertex/Events");
+   tmuons = (TTree*)f->Get("mymuons/Events");
+   ttaus = (TTree*)f->Get("mytaus/Events");
+   tmets = (TTree*)f->Get("mymets/Events");
+   //Make friends so we can have access to friends variables	
+   //we may not use all of the available information
+   //it is just an example
+   tree->AddFriend(tevents);
+   tree->AddFriend(tvertex);
+   tree->AddFriend(tmuons);
+   tree->AddFriend(ttaus);
+   tree->AddFriend(tmets);
    Init(tree);
 }
 
@@ -109,10 +153,9 @@ Int_t EventLoopAnalysisTemplate::GetEntry(Long64_t entry)
 }
 
 
-
 Long64_t EventLoopAnalysisTemplate::LoadTree(Long64_t entry)
 {
-// Set the environment to read one entry
+  //cout<<" Set the environment to read one entry"<<endl;
    if (!fChain) return -5;
    Long64_t centry = fChain->LoadTree(entry);
    if (centry < 0) return centry;
@@ -120,6 +163,7 @@ Long64_t EventLoopAnalysisTemplate::LoadTree(Long64_t entry)
       fCurrent = fChain->GetTreeNumber();
       Notify();
    }
+
    return centry;
 }
 
@@ -135,21 +179,24 @@ void EventLoopAnalysisTemplate::Init(TTree *tree)
    // (once per file to be processed).
 
    // Set object pointer
-   muon_e = 0;
+   //In our case, only vectors and maps
+   triggermap =0;
    muon_pt = 0;
-
+  
    // Set branch addresses and branch pointers
    if (!tree) return;
    fChain = tree;
    fCurrent = -1;
-   fChain->SetMakeClass(1);
+   //Comment out to be able to read map
+   //https://root-forum.cern.ch/t/std-map-in-ttree-with-makeclass/14171
+   //fChain->SetMakeClass(1);
 
    fChain->SetBranchAddress("run", &run, &b_run);
    fChain->SetBranchAddress("luminosityBlock", &luminosityBlock, &b_luminosityBlock);
    fChain->SetBranchAddress("event", &event, &b_event);
-   fChain->SetBranchAddress("muon_e", &muon_e, &b_muon_e);
+   fChain->SetBranchAddress("PV_npvs", &PV_npvs, &b_PV_npvs);
+   fChain->SetBranchAddress("triggermap",&triggermap,&b_triggermap);
    fChain->SetBranchAddress("muon_pt", &muon_pt, &b_muon_pt);
-   
    Notify();
 }
 
@@ -173,35 +220,26 @@ void EventLoopAnalysisTemplate::Show(Long64_t entry)
    fChain->Show(entry);
 }
 
-Int_t EventLoopAnalysisTemplate::Cut(Long64_t entry)
-{
-// This function may be called from Loop.
-// returns  1 if entry is accepted.
-// returns -1 otherwise.
-   return 1;
-}
-
 void EventLoopAnalysisTemplate::Loop()
 {
   if (fChain == 0) return;
 
-    Long64_t nentries = fChain->GetEntriesFast();
-
-    Long64_t nbytes = 0, nb = 0;
-    for (Long64_t jentry=0; jentry<nentries;jentry++) {
-	//Just an informative printout
-	if(jentry%1000 == 0) {
-	      cout<<"Processed "<<jentry<<" events out of "<<nentries<<endl;
-	} 
-       //Load the current event	
-       Long64_t ientry = LoadTree(jentry);
-       if (ientry < 0) break;
-       nb = fChain->GetEntry(jentry);   nbytes += nb;
-       // if (Cut(ientry) < 0) continue;
-       //Perform the analysis
-       analysis();
-
-    }
+  Long64_t nentries = fChain->GetEntriesFast();
+  
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    //Just an informative printout (change for more or less printing)
+    if(jentry%10 == 0) {
+      cout<<"Processed "<<jentry<<" events out of "<<nentries<<endl;
+    } 
+    //cout<<"Load the current event"<<endl;
+    Long64_t ientry = LoadTree(jentry);
+    if (ientry < 0) break;
+    nb = fChain->GetEntry(jentry);   nbytes += nb;
+    
+    analysis();
+    
+  }
 }
 
 
@@ -210,26 +248,55 @@ void EventLoopAnalysisTemplate::analysis()
 {
 //-----------------------------------------------------------------
 
-   //cout<<"Make histogram of lumi blocks"<<endl;
-   lumib->Fill(luminosityBlock);	
+  //cout<<"analysis() execution"<<endl;
 
-   //Loop over muons container and fill histo if cut is passed
-   Int_t nmuons = muon_pt->size();
-   cout<<"Number of muons = "<<nmuons<<endl;
-   h_nmu->Fill(nmuons);
-   float mu_pt_cut = 20; //in GeV
-   if(nmuons>0){
-     for (Int_t j=0; j<nmuons;++j){
-       cout<<"Muon pT = "<<muon_pt->at(j)<<endl;
-       if (muon_pt->at(j)>mu_pt_cut){
-	 h_mu_pt->Fill(muon_pt->at(j));
-	 h_mu_e->Fill(muon_e->at(j));
-       }
-     }
-   }
+  //minimal selection with trigger requirement
+  //if (!MinimalSelection()) return;
+  
+  //fill histogram
+  Int_t histsize = sizeof(hists)/sizeof(hists[0]);
+  for (Int_t j=0;j<histsize;++j){
+    
+    TString histname = TString(hists[j]->GetName());
+    TString thelabel = histname(0,histname.First("_"));
+    TString thevar = histname(histname.First("_")+1,histname.Sizeof());
+    
+    if (thelabel == labeltag){ 
+      //primary vertices
+      if(thevar == "npv"){
+	hists[j]->Fill(PV_npvs,theweight);
+      }
+    }
+  }
+  
 
 
-}
+}//------analysis()
+
+/*
+ * Perform a selection on the minimal requirements of an event
+ */
+//-----------------------------------------------------------------
+bool EventLoopAnalysisTemplate::MinimalSelection()
+{
+//-----------------------------------------------------------------
+
+  //cout<<"Applying minimal selection"<<endl;
+  bool isTrigger = false;
+
+  //Check trigger and acceptance bit
+  for (map<string, int>::iterator it=triggermap->begin();it!=triggermap->end();it++){
+    if(it->first.find(triggerRequest)!=string::npos &&
+       it->second!=0){
+	 //cout<<it->first<<"  "<<it->second<<endl;
+      isTrigger = true;
+    }
+  }
+
+ 
+  return isTrigger;
+
+}//------MinimalSelection
 
 
 //-----------------------------------------------------------------
@@ -237,21 +304,52 @@ int main()
 {
 //-----------------------------------------------------------------
 
-    cout<<"Build the analysis object"<<endl;
-    EventLoopAnalysisTemplate mytemplate;
+  //To be able to read the trigger map
+  gROOT->ProcessLine("#include<map>");
 
+  
+  //Compute event weights to be used for the respective datasets
+  //const float integratedLuminosity = 4.412 * 1000.0; // Run2012B only
+  //const float integratedLuminosity = 7.055 * 1000.0; // Run2012C only
+  //const float integratedLuminosity = 11.467 * 1000.0; // Run2012B+C
+  //const float mc_w = <xsec> / <#events> * integratedLuminosity;
+  const float data_w = 1.0;
+  
+  map<string, pair<string,float> > sampleNames;
+  //sampleNames.insert(make_pair("MCSample",make_pair("mc",mc_w)));
+  sampleNames.insert(make_pair("myoutput",make_pair("data",data_w)));
+  
+  //loop over sample files with names  defined above
+  for(map< string,pair<string,float> >::iterator it=sampleNames.begin();
+      it!=sampleNames.end();it++){
+    
+    TString samplename = it->first;
+    TString thelabel = it->second.first;
+    Float_t sampleweight = it->second.second;
+    
+    TStopwatch time;
+    time.Start();
+    cout << ">>> Processing sample " << samplename <<" with label "<<thelabel<<" and weight "<<sampleweight<<":" <<endl;
+    
+    TString filename = samplesBasePath+samplename+".root";
+    
+    cout<<"Build the analysis object with file "<<filename<<endl;
+    EventLoopAnalysisTemplate mytemplate(filename,thelabel,sampleweight);
+    
     cout<<"Run the event loop"<<endl;
     mytemplate.Loop();
+    time.Stop();
+    time.Print();
+  }
 
-    cout<<"Save the histograms"<<endl;
-    TFile* hfile = new TFile("histograms.root","RECREATE");
-    lumib->Write();
-    h_nmu->Write();
-    h_mu_e->Write();
-    h_mu_pt->Write();
-    hfile->Close();
+  TFile* hfile = new TFile("histograms.root","RECREATE");
 
-    return 1;
+  //Save signal region histos
+  data_npv->Write();
+  
+  hfile->Close();
+
+  return 0;
 
 }
 
