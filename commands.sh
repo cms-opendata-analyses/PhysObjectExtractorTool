50b64f83a33ed5f7f9011ddc7f3a072afcc874dc
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-11-30 20:37:01 +0100

    Update workflow files

--- commands.sh
+++ commands.sh
@@ -1,17 +1,31 @@
-#!/bin/sh -l
-# exit on erro
+# parameters: $1 runas, $2 config, $3 isData, $4 doPat
+# if running outside github actions, give any other 1st paramater than github i.e. commands.sh mywork (or omit the paramaters)
+# defaults:
+if [ -z "$1" ]; then runas=other; else runas=$1; fi
+if [ -z "$2" ]; then config=python/poet_cfg.py; else config=$2; fi
+if [ -z "$3" ]; then isData=False; else isData=$3; fi
+if [ -z "$4" ]; then doPat=False; else doPat=$4; fi
+
 set -e
-sudo chown $USER /mnt/vol
 
 # For the plain github action with docker, the area would be available in /mnt/vol
-cp -r /mnt/vol PhysObjectExtractorTool
+if [ $runas = github ]
+then
+  sudo chown $USER /mnt/vol
+  cp -r /mnt/vol PhysObjectExtractorTool
+else
+  git clone -b 2011 https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
+fi
+
 cd PhysObjectExtractorTool
 cd PhysObjectExtractor
 scram b
 
-# sed -i "s/process.GlobalTag.connect/#process.GlobalTag.connect/g" python/poet_cfg.py
-cmsRun python/poet_cfg.py
+cmsRun $config $isData $doPat
 
-cp *.root /mnt/vol/
-echo ls -l /mnt/vol
-ls -l /mnt/vol
+if [ $runas = github ]
+then
+  cp *.root /mnt/vol/
+  echo ls -l /mnt/vol
+  ls -l /mnt/vol
+fi  
